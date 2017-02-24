7c1cb538a35751741a254b459c57a413be5bc883
Steve <xenophone2@hotmail.com>
2017-02-24 13:59:45 -0800

    Update formatting.py

--- main/formatting.py
+++ main/formatting.py
@@ -8,42 +8,33 @@ from django.core.context_processors import csrf
 from django.views.decorators.csrf import csrf_exempt
 import ast
 
+def format_handler(content, request):
+    processed_content = []
+    for i in content:
+        if type(i) == dict:
+            processed_dict = choice_selector(i, request)
+            processed_content.append(processed_dict)
+        elif type(i) == str:
+            processed_content.append(i)
+    #new_content = "<p>" + " ".join(processed_content) + "</p>"
+    new_content = " ".join(processed_content)
+    return new_content
 
 
-'''def save_game(text, request):
+
+def choice_selector(dict_choices, request):
     user = request.user
     profile = user.profile
-    str1 = '<but'
-    str2 = '<field'
-    if text.find(str1):
-        save_body, save_end = text[:text.find(str1)], text[text.find(str1):]
-        profile.textfield = profile.textfield + save_body
-        profile.textend = save_end
-        profile.save()
-    elif text.find(str2):
-        save_body, save_end = text[:text.find(str2)], text[text.find(str2):]
-        profile.textfield = profile.textfield + save_body
-        profile.textend = save_end
-        profile.save()
-    else:
-        profile.textfield = profile.textfield + text
-        profile.save()'''
+    attrs = profile.attrs
+    for attr in attrs:
+        for key, value in dict_choices.iteritems():
+            if attr == key:
+                choice = dict_choices[key]
+                if type(choice) == dict:
+                    return choice_selector(choice,request)
+                else:
+                    return choice
 
-def formatter(content, request):
-    if content.find('[') >= 0:
-        user = request.user
-        profile = user.profile
-        attrs = profile.attrs
-        ind, indend = content.find('['), content.find(']')
-        choice = ast.literal_eval(content[ind+1:indend])
-        for attr in attrs:
-            for key, value in choice.iteritems():
-                if attr == key:
-                    new_choice = choice[key]
-                    new_string = content[:ind] + new_choice + content[indend+1:]
-                    return formatter(new_string, request)
-    else:
-        return content
 
 def pronouns_format(choice, request):
     user = request.user
@@ -51,13 +42,13 @@ def pronouns_format(choice, request):
     if choice==profile.sex:
         if profile.sex=='male':
             gender_nouns = {'hisher':'his', 'heshe':'he', 'himselfherself':'himself', 'heshe':'he', 'himher':'him', 'hisherS': 'his', \
-            'Chisher':'His', 'Cheshe':'He', 'Chimselfherself':'Himself', 'Cheshe':'He', 'Chimher':'Him', 'ChisherS': 'His'}
+            'Chisher':'His', 'Cheshe':'He', 'Chimselfherself':'Himself', 'Cheshe':'He', 'Chimher':'Him', 'ChisherS': 'His', 'manwom': 'man'}
             profile.gender_nouns = gender_nouns
             profile.save()
             return gender_nouns
         elif profile.sex=='female':
             gender_nouns = {'hisher':'her', 'heshe':'she', 'himselfherself':'herself', 'heshe':'she', 'himher':'her', 'hisherS': 'hers',  \
-            'Chisher':'Her', 'Cheshe':'She', 'Chimselfherself':'Herself', 'Cheshe':'She', 'Chimher':'Her', 'ChisherS': 'Hers'}
+            'Chisher':'Her', 'Cheshe':'She', 'Chimselfherself':'Herself', 'Cheshe':'She', 'Chimher':'Her', 'ChisherS': 'Hers', 'manwom': 'lady'}
             return gender_nouns
 
 
@@ -102,14 +93,14 @@ def parse_request(request):
             attrs.append(str(request.POST.get('hair_style')))
             profile.attrs = attrs
             profile.save()
-    name, action = str(request.POST.get('name')), str(request.POST.get('action'))
+    name, action = str(request.POST.get('name')), str(request.POST.get('action')) #From POST request get name and action from html button
     gender_nouns = pronouns_format(profile.sex, request)
-    text_retrieve = act1[name][action]
-    test_text = formatter(text_retrieve, request)
+    text_retrieve = act1[name][action] #Use name and action from POST request button attributes to retrieve next content from Content.
+    test_text = format_handler(text_retrieve, request) #Pass new content through formatter to remove '['s and format output
     text = test_text.format(  \
                                       charname=profile.charname, himher=gender_nouns['himher'], heshe=gender_nouns['heshe'], \
                                       hisher=gender_nouns['hisher'], himselfherself=gender_nouns['himselfherself'], hishers=gender_nouns['hisherS'], Chimher=gender_nouns['Chimher'], \
-                                      Cheshe=gender_nouns['Cheshe'], Chisher=gender_nouns['Chisher'], Chimselfherself=gender_nouns['Chimselfherself'], Chishers=gender_nouns['ChisherS'],)
+                                      Cheshe=gender_nouns['Cheshe'], Chisher=gender_nouns['Chisher'], Chimselfherself=gender_nouns['Chimselfherself'], Chishers=gender_nouns['ChisherS'], manwom=gender_nouns['manwom'],)
     #test_text = formatter(text)
     #save_game(text, request)
     profile.textfield = text
